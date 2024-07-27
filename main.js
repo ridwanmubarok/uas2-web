d9a58fb962c1c830f51801f00d8b61c01de7b089
amubhya <43973623+ridwanmubarok@users.noreply.github.com>
2024-07-27 21:14:02 +0700

    update: style

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-undef */
 import './src/assets/style.css'
 import 'remixicon/fonts/remixicon.css'
 import Swiper from 'swiper/bundle'
@@ -20,4 +21,28 @@ export const swiperTrending = new Swiper('.swiper-trending', {
     delay: 3000,
   },
   slidesPerView: 1,
+})
+
+let isPlaying = false
+
+const audioElement = document.getElementById('audio')
+const audioContext = new (window.AudioContext || window.webkitAudioContext)()
+const track = audioContext.createMediaElementSource(audioElement)
+
+track.connect(audioContext.destination)
+
+document.getElementById('play').addEventListener('click', function () {
+  if (!isPlaying) {
+    document.getElementById('play').classList.remove('ri-play-circle-line')
+    document.getElementById('play').classList.add('ri-pause-circle-line')
+    audioContext.resume().then(() => {
+      audioElement.play()
+      isPlaying = true
+    })
+  } else {
+    document.getElementById('play').classList.remove('ri-pause-circle-line')
+    document.getElementById('play').classList.add('ri-play-circle-line')
+    audioElement.pause()
+    isPlaying = false
+  }
 })
